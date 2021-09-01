bbd767f322fb0bdd06feeedd091f193a9cb4bd35
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-09-01 13:33:29 +0300

    finishing onBoarding screen

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,28 +1,30 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button} from 'react-native'
+import { StyleSheet, Image} from 'react-native'
 import Onboarding from 'react-native-onboarding-swiper';
 
 const OnboardingScreen = ({navigation}) => {
  return (
   <Onboarding
+   let onSkip = {() => navigation.replace('Login')}
+   let onDone = {() => navigation.navigate('Login')}
    pages={[
-     {
+     { 
        backgroundColor: '#fff',
-       image: <Image source={require('./images/circle.png')} />,
-       title: 'Onboarding',
-       subtitle: 'Done with React Native Onboarding Swiper',
+       image: <Image source={require('../assets/img-1.png')} />,
+       title: 'Connect to the world',
+       subtitle: 'The new way to connect to the world',
      },
      {
        backgroundColor: '#fff',
-       image: <Image source={require('./images/circle.png')} />,
-       title: 'Onboarding',
-       subtitle: 'Done with React Native Onboarding Swiper',
+       image: <Image source={require('../assets/img-2.png')} />,
+       title: 'Share your Favorites',
+       subtitle: 'Share your things with similar people from the world',
      },
      {
        backgroundColor: '#fff',
-       image: <Image source={require('./images/circle.png')} />,
-       title: 'Onboarding',
-       subtitle: 'Done with React Native Onboarding Swiper',
+       image: <Image source={require('../assets/img-3.png')} />,
+       title: 'Become a star',
+       subtitle: 'Let the spot light capture you ',
      },
    ]}
  />
